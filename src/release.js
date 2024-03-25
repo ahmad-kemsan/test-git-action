e8abb053280f47ac97037c314cfe4465702935f0
ahmad-kemsan <ahmadkemsan@gmail.com>
2024-03-25 15:47:55 +0530

    chore: update git action

--- src/release.js
+++ src/release.js
@@ -7,8 +7,9 @@ export function releaseVersion(newVersion, repoUrl, token) {
   console.log(`Releasing with version: ${newVersion}`);
   execSync(`git config user.name "ahmad-kemsan"`);
   execSync(`git config user.email "ahmadkemsan@gmail.com"`);
-  execSync(`git commit --allow-empty -m "chore: inside action empty commit version bump"`);
+  execSync(`git commit --allow-empty -m "chore: inside action empty commit version bump"
+  Release-As: ${newVersion}`);
   execSync(`git push`);
 
-  execSync(`npx release-please release-pr --release-as ${newVersion} --repo-url ${repoUrl} --token ${token} --skip-labeling`, { stdio: 'inherit' });
+  execSync(`npx release-please release-pr --repo-url ${repoUrl} --token ${token} --skip-labeling`, { stdio: 'inherit' });
 }
