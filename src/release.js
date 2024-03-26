002bcfdcf1a8513500cb4f65ebe105746f75384f
ahmad-kemsan <ahmadkemsan@gmail.com>
2024-03-26 08:01:26 +0530

    chore: update commit pattern

--- src/release.js
+++ src/release.js
@@ -4,11 +4,11 @@ import core from '@actions/core';
 export function releaseVersion(newVersion, repoUrl, token) {
   core.setOutput('released-version', newVersion.version);
 
-  console.log(`Releasing with version: ${newVersion}`);
+  console.log(`Releasing with version1: ${newVersion}`);
   execSync(`git config user.name "ahmad-kemsan"`);
   execSync(`git config user.email "ahmadkemsan@gmail.com"`);
-  execSync(`git commit --allow-empty -m "chore: inside action empty commit version bump"`);
-  execSync(`git commit --allow-empty -m "Release-As: ${newVersion}"`);
+  execSync(`git commit --allow-empty -m "chore: inside action empty commit version bump (Release-As: ${newVersion}")"`);
+  
   execSync(`git push`);
 
   execSync(`npx release-please release-pr --repo-url ${repoUrl} --token ${token} --skip-labeling`, { stdio: 'inherit' });
